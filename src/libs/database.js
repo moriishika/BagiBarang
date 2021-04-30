e41571a7562b4ab7377d0fff20fc1d0ea694846d
ramanavami <ramanavami32@gmail.com>
2021-04-30 18:17:41 +0800

    trying to fix merging issue

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import * from 'dotenv'
 const connectionString = process.env.MONGODB_URI
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
