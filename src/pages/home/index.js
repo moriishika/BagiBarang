975c6cad67885e68cab6d36cb2630270b1f32458
ramanavami <ramanavami32@gmail.com>
2021-04-30 22:22:28 +0800

    added home folder trying to fix vercel error

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import { TopNavbar, Items } from '../../components'
+
+const Home = () => {
+    return (
+        <main className="h-auto w-full">
+            <TopNavbar></TopNavbar>
+            <Items ></Items>
+        </main>
+    )
+}
+
+export default Home;
