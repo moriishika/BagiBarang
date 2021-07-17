ffb9198523ddef3bb7fcb5db1e4cae67557c9cdc
ramanavami <ramanavami32@gmail.com>
2021-07-17 12:41:32 +0800

    added image to detail

--- src/pages/items/[id].js
+++ src/pages/items/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import ItemModel from '../../models/Items';
+import Image from 'next/image';
 
 const Item = (props) => {
     const router = useRouter()
@@ -17,6 +18,9 @@ const Item = (props) => {
 
             <main>
                 <h1>{item.name}</h1>
+                <div className="w-48 h-72 relative">
+                    <Image layout="fill" src={`/assets/images/items/${item.images[0]}`}></Image>
+                </div>
                 <h1 className="font-bold text-red-600">{item.name}</h1>
             </main>
         </div>
