f75e3daf60217b3c717481ce439f45531f2815cf
ramanavami <ramanavami32@gmail.com>
2021-07-19 09:38:46 +0800

    changed carousel lib with react-slick

--- src/components/MediaSlider/MediaSlider.js
+++ src/components/MediaSlider/MediaSlider.js
@@ -1,56 +1,36 @@
-import React from "react";
-import { useKeenSlider } from "keen-slider/react";
-import "keen-slider/keen-slider.min.css";
+import { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const MediaSlider = (props) => {
-  const [currentSlide, setCurrentSlide] = React.useState(0);
-  const [sliderRef, slider] = useKeenSlider({
-    initial: 0,
-    slideChanged(s) {
-      setCurrentSlide(s.details().relativeSlide);
-    },
-  });
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    lazyLoad : true,
+    customPaging: i => (
+      <div className="w-3.5 border-2 h-3.5  bg-white border-black rounded-full"></div>
+    )
+  };
+
   return (
     <div>
-      <div ref={sliderRef} className="keen-slider rounded-xl">
-        {props.images.map((image, index) => {
+      <Slider {...settings}>
+        {props.images.map((image) => {
           return (
-            <div
-              className={`keen-slider__slide number-slide${index} rounded-xl flex justify-center items-center`}
-            >
-              <div className="h-auto">
-                <img
-                  src={`/assets/images/items/${image}`}
-                  className="rounded-xl"
-                />
-              </div>
+            <div>
+              <img
+                src={`/assets/images/items/${image}`}
+                className="rounded-xl m-auto"
+              />
             </div>
           );
         })}
-      </div>
-
-      <div className="flex w-full h-6 justify-center items-center">
-        {slider && (
-          <div className="dots">
-            {[...Array(slider.details().size).keys()].map((idx) => {
-              return (
-                <button
-                  key={idx}
-                  onClick={() => {
-                    slider.moveToSlideRelative(idx);
-                  }}
-                  className={
-                    "w-3.5 border-2 h-3.5 mx-1 mt-1 rounded-full" +
-                    (currentSlide === idx
-                      ? " bg-white border-black"
-                      : " bg-black")
-                  }
-                />
-              );
-            })}
-          </div>
-        )}
-      </div>
+      </Slider>
     </div>
   );
 };
